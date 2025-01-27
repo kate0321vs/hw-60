852f36c217c48e71ebd612561858992d0c6aac16
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-01-27 14:14:15 +0600

    Создала контейнер Chat и компонент формы для отправки сообщения

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Chat from './containers/Chat/Chat.tsx';
 
 const App = () => {
 
   return (
     <>
+      <Chat/>
     </>
   )
 };
---
+++ src/containers/Chat/Chat.tsx
@@ -0,0 +1,12 @@
+import UserForm from '../../components/UserForm/UserForm.tsx';
+
+const Chat = () => {
+
+  return (
+    <div>
+      <UserForm/>
+    </div>
+  );
+};
+
+export default Chat;
