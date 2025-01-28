7c753e2c30a9954f27df433d657c392fb5f60a68
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-01-28 16:07:29 +0600

    Добавила в UseEffect запрос для добавления новых сообщений отоносительно даты последнего сообщения

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -5,62 +5,70 @@ import UserMessages from '../../components/UserMessages/UserMessages.tsx';
 
 const url = 'http://146.185.154.90:8000/messages'
 
+const formatDate = (date: string): string => {
+  const dateTime = new Date(date);
+  let month = (dateTime.getMonth() + 1).toString(),
+    day = dateTime.getDate().toString(),
+    hours = dateTime.getHours().toString(),
+    minutes = dateTime.getMinutes().toString(),
+    seconds = dateTime.getSeconds().toString();
+  const year = dateTime.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+  if (hours.length < 2) hours = '0' + hours;
+  if (minutes.length < 2) minutes = '0' + minutes;
+  if (seconds.length < 2) seconds = '0' + seconds;
+
+  return `${day}.${month}.${year} at ${hours}:${minutes}:${seconds}`;
+};
+
 const Chat = () => {
   const [userMessages, setUserMessages] = useState<Messages[]>([]);
-
-  const formatDate = (date: string): string => {
-    const dateTime = new Date(date);
-    let month = (dateTime.getMonth() + 1).toString(),
-      day = dateTime.getDate().toString(),
-      hours = dateTime.getHours().toString(),
-      minutes = dateTime.getMinutes().toString(),
-      seconds = dateTime.getSeconds().toString(),
-      year = dateTime.getFullYear();
-
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-    if (hours.length < 2) hours = '0' + hours;
-    if (minutes.length < 2) minutes = '0' + minutes;
-    if (seconds.length < 2) seconds = '0' + seconds;
-
-    return `${day}.${month}.${year} at ${hours}:${minutes}:${seconds}`;
-  };
+  const [lastDateTimeOfMessage, setLastDateTimeOfMessage] = useState<string | null>(null)
 
   useEffect(() => {
     const fetchData = async () => {
+      const fetchUrl = lastDateTimeOfMessage ?
+        `http://146.185.154.90:8000/messages?datetime=${lastDateTimeOfMessage}` : url
 
-      const response = await fetch(url);
+      const response = await fetch(fetchUrl);
       if (response.ok) {
         const posts = await response.json() as Messages[];
         console.log('posts', posts);
-        const formattedPosts = posts.map(post => {
-          return {
-            _id: post._id,
-            author: post.author,
-            message: post.message,
-            datetime: formatDate(post.datetime)
-          };
-
-        });
-        console.log('formattedPosts', formattedPosts)
-        setUserMessages(formattedPosts);
+        if (posts.length > 0) {
+          const responseMessages = posts.map(post => {
+            return {
+              _id: post._id,
+              author: post.author,
+              message: post.message,
+              datetime: formatDate(post.datetime)
+            };
+          });
+          responseMessages.reverse();
+          setLastDateTimeOfMessage(posts[posts.length - 1].datetime);
+          setUserMessages(prevState => [...responseMessages, ...prevState]);
+        }
       }
     };
     void fetchData();
-  }, []);
-  console.log('messages', userMessages);
+
+    const interval = setInterval(async () => {
+      void fetchData();
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [lastDateTimeOfMessage]);
 
   return (
     <div>
       <UserForm/>
       {userMessages.length > 0 ?
-          <>
-            <UserMessages userMessages={userMessages}/>
-          </>
+        <>
+          <UserMessages userMessages={userMessages}/>
+        </>
         :
-          <p className="w-75 mx-auto">No Messages</p>
+        <p className="w-75 mx-auto">No Messages</p>
       }
-
     </div>
   );
 };
